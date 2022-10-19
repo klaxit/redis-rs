7ca478de513165d81a3df916bee5db017016fe3c
Quentin Wentzler <quentin.wentzler@klaxit.com>
2022-10-19 10:11:20 +0200

    init a empty hash when encountering a nil value
    during hash value generation.

--- src/types.rs
+++ src/types.rs
@@ -1109,10 +1109,16 @@ impl<K: FromRedisValue + Eq + Hash, V: FromRedisValue, S: BuildHasher + Default>
     for HashMap<K, V, S>
 {
     fn from_redis_value(v: &Value) -> RedisResult<HashMap<K, V, S>> {
-        v.as_map_iter()
-            .ok_or_else(|| invalid_type_error_inner!(v, "Response type not hashmap compatible"))?
-            .map(|(k, v)| Ok((from_redis_value(k)?, from_redis_value(v)?)))
-            .collect()
+        match *v {
+            Value::Nil => Ok(HashMap::<K, V, S>::default()),
+            _ => v
+                .as_map_iter()
+                .ok_or_else(|| {
+                    invalid_type_error_inner!(v, "Response type not hashmap compatible")
+                })?
+                .map(|(k, v)| Ok((from_redis_value(k)?, from_redis_value(v)?)))
+                .collect(),
+        }
     }
 }
 
